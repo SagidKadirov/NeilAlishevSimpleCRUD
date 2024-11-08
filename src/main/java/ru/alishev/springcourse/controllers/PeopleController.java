f9942a31d458288e8c1f5fb6177f9d0640a63adc
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-08 21:53:22 +0500

    people/id/edit doesn't work with ParchMapping annotation

--- src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -34,7 +34,7 @@ public class PeopleController {
     }
 
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person")Person person) {
+    public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
 
@@ -43,4 +43,19 @@ public class PeopleController {
         personDAO.save(person);
         return "redirect:/people";
     }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person,
+                         @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+
 }
--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -1,6 +1,5 @@
 package ru.alishev.springcourse.dao;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import ru.alishev.springcourse.models.Person;
 
@@ -13,26 +12,31 @@ public class PersonDAO {
     private List<Person> people;
 
     {
-        people=new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Tom"));
-        people.add(new Person(++PEOPLE_COUNT,"Bob"));
-        people.add(new Person(++PEOPLE_COUNT,"Mike"));
-        people.add(new Person(++PEOPLE_COUNT,"Katy"));
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
     }
 
-    public List<Person>index(){
+    public List<Person> index() {
         return people;
     }
 
-    public Person show(int id){
+    public Person show(int id) {
         return people.stream()
-                .filter(p->p.getId()==id)
+                .filter(p -> p.getId() == id)
                 .findAny()
                 .orElse(null);
     }
 
-    public void save(Person person){
+    public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    public void update(int id, Person person) {
+        Person personToUpdate=show(id);
+        personToUpdate.setName(person.getName());
     }
 }
