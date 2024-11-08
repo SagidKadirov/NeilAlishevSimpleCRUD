4042c7cfa4c1f17d2ad91c80d4319bf131efbf36
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-08 20:30:32 +0500

    new Person creating page built (id autoincrement)

--- src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.alishev.springcourse.dao.PersonDAO;
+import ru.alishev.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -30,7 +29,19 @@ public class PeopleController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
 //        Получим одного человека по его id из DAO и передадим на отображение в представление
-        model.addAttribute("person",personDAO.show(id));
+        model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
