89a817bda1668dd86e3d51b9f4595497427b223f
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-10 01:19:17 +0500

    added age and email string and added the hibernate validator engine

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -13,10 +13,10 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom",24,"tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob",52,"bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18,"mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy",34,"katy@gmail.com"));
     }
 
     public List<Person> index() {
@@ -35,9 +35,12 @@ public class PersonDAO {
         people.add(person);
     }
 
-    public void update(int id, Person person) {
+    public void update(int id, Person updatedPerson) {
         Person personToUpdate=show(id);
-        personToUpdate.setName(person.getName());
+
+        personToUpdate.setName(updatedPerson.getName());
+        personToUpdate.setAge(updatedPerson.getAge());
+        personToUpdate.setEmail(updatedPerson.getEmail());
     }
     public void delete(int id){
         people.removeIf(person -> person.getId()==id);
--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -1,15 +1,46 @@
 package ru.alishev.springcourse.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
     private int id;
-    private String name;
 
-    public Person(int id, String name) {
+    @NotEmpty(message="The name should not be empty")
+    @Size(min=2, max=30, message = "The name should be between 2 and 30 characters")
+    private String name;
+    @Min(value = 0,message = "Age should be greater than 0")
+    private int age;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.age = age;
+        this.email = email;
         this.id = id;
         this.name = name;
     }
 
-    public Person() {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public int getId() {
